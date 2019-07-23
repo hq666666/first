30a240c64843cc5f95eb8e76713a5163bf57171c
hq <18096652696@163.com>
2019-07-24 04:21:05 +0800

    项目搭建第一天

---
+++ user-service/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name){
+        return  "this is my name:"+name;
+    }
+}
